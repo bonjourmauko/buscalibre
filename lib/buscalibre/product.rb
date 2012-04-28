f62577c92ce3ebb437ca5fe435222d42ce40748c
maukoquiroga <mauko@pictorical.com>
2012-04-28 18:21:42 -0300

    within

--- lib/buscalibre/product.rb
+++ lib/buscalibre/product.rb
@@ -9,10 +9,44 @@ module Buscalibre
       body     = Yajl::Parser.parse( response.body )
 
       # if the product exists
-      return body.first.recursive_symbolize_keys! if body.instance_of? Array
+      return body.first.recursive_symbolize_keys! if body.instance_of?(Array)
 
       # else
-      return body.recursive_symbolize_keys!
+      return nil
+    end
+
+    def self.within range = 1..10, opts = { validate: true }
+      Array.new.tap do |products|
+        range.each do |id|
+          product = self.find id
+          products.push product if product and not opts[:validate]
+          products.push product if product and opts[:validate] and self.valid?(product)
+        end
+      end
+    end
+
+    def self.valid? product
+      return false unless product[:nombre]
+      return false unless product[:descripcion]
+      return false unless product[:currency]
+      return false unless product[:country]
+      return false unless product[:price]
+      return false unless product[:images]
+      return false unless product[:images].first
+      return false unless product[:categorias]
+      return false unless product[:categorias][:categoria]
+      return false unless product[:categorias][:categoria][:id]
+      return false unless product[:categorias][:categoria][:url]
+      return false unless product[:categorias][:categoria][:nombre]
+      return false unless product[:variaciones]
+      return false unless product[:variaciones].first
+      return false unless product[:variaciones].first[:proveedores]
+      return false unless product[:variaciones].first[:proveedores].first
+      return false unless product[:variaciones].first[:proveedores].first[:stock]
+      return false unless product[:variaciones].first[:proveedores].first[:precio_en_destino]
+      return false unless product[:variaciones].first[:proveedores].first[:dias_transporte]
+      return false unless product[:variaciones].first[:proveedores].first[:disponibilidad]
+      return true
     end
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,35 +2,54 @@ require 'spec_helper'
 
 describe Buscalibre::Product do
   # we need to succesfully load the class variables before each test
-  let!(:base)             { Buscalibre::Base.new('development', Dir.pwd) }
-  let!(:valid_product)    { Buscalibre::Product.find 118 }
-  let!(:invalid_product)  { Buscalibre::Product.find 10000000000000 }
+  let!(:base) { Buscalibre::Base.new('development', Dir.pwd) }
 
-  context "given we have a valid product id" do
-    it { valid_product.should be_an_instance_of  Hash    }
-    it { valid_product[:id_producto].to_i.should ==  118 }
-    it { valid_product[:nombre].should_not        be  nil }
-    it { valid_product[:descripcion].should_not   be  nil }
-    it { valid_product[:currency].should_not      be  nil }
-    it { valid_product[:country].should_not       be  nil }
-    it { valid_product[:price].should_not         be  nil }
-    it { valid_product[:images].first.should_not  be  nil }
-    it { valid_product[:categorias][:categoria].should_not                                                      be  nil }
-    it { valid_product[:categorias][:categoria][:childs][:categoria].should_not                                 be  nil }
-    it { valid_product[:categorias][:categoria][:childs][:categoria][:childs][:categoria].should_not            be  nil }
-    it { valid_product[:categorias][:categoria][:childs][:categoria][:childs][:categoria][:childs].first.should be  nil }
-    it { valid_product[:variaciones].first.should_not                                           be  nil }
-    it { valid_product[:variaciones].first[:proveedores].first.should_not                       be  nil }
-    it { valid_product[:variaciones].first[:proveedores].first[:stock].should_not               be  nil }
-    it { valid_product[:variaciones].first[:proveedores].first[:precio_en_destino].should_not   be  nil }
-    it { valid_product[:variaciones].first[:proveedores].first[:dias_transporte].should_not     be  nil }
-    it { valid_product[:variaciones].first[:proveedores].first[:disponibilidad].should_not      be  nil }
+  context "find", :find do
+    context "given we have an existing product id" do
+      let!(:existing_product) { Buscalibre::Product.find 118 }
+
+      it { existing_product.should be_an_instance_of   Hash    }
+      it { existing_product[:id_producto].to_i.should  ==  118 }
+      it { existing_product[:nombre].should_not        be  nil }
+      it { existing_product[:descripcion].should_not   be  nil }
+      it { existing_product[:currency].should_not      be  nil }
+      it { existing_product[:country].should_not       be  nil }
+      it { existing_product[:price].should_not         be  nil }
+      it { existing_product[:images].first.should_not  be  nil }
+      it { existing_product[:categorias][:categoria].should_not                                                      be  nil }
+      it { existing_product[:categorias][:categoria][:childs][:categoria].should_not                                 be  nil }
+      it { existing_product[:categorias][:categoria][:childs][:categoria][:childs][:categoria].should_not            be  nil }
+      it { existing_product[:categorias][:categoria][:childs][:categoria][:childs][:categoria][:childs].first.should be  nil }
+      it { existing_product[:variaciones].first.should_not                                           be  nil }
+      it { existing_product[:variaciones].first[:proveedores].first.should_not                       be  nil }
+      it { existing_product[:variaciones].first[:proveedores].first[:stock].should_not               be  nil }
+      it { existing_product[:variaciones].first[:proveedores].first[:precio_en_destino].should_not   be  nil }
+      it { existing_product[:variaciones].first[:proveedores].first[:dias_transporte].should_not     be  nil }
+      it { existing_product[:variaciones].first[:proveedores].first[:disponibilidad].should_not      be  nil }
+    end
+
+    context "given we have a non-existing product id" do
+      let!(:non_existing_product) { Buscalibre::Product.find 10000000000000 }
+
+      it { non_existing_product.should be_an_instance_of NilClass }
+    end
   end
 
-  context "given we have an invalid product id" do
-    it { invalid_product.should be_an_instance_of Hash }
-    it { invalid_product[:success].to_i.should == 0    }
-    it { invalid_product[:code].to_i.should    == 2    }
-    it { invalid_product[:message].should      == "No se encontro el producto solicitado" }
+  context "within", :within do
+    it { Buscalibre::Product.within(1..10, validate: false).size.should_not == 0 }
+    it { Buscalibre::Product.within(1..10, validate: true).size.should      == 0 }
+  end
+
+  context "valid?", :valid? do
+    let!(:valid_product) { Buscalibre::Product.find 118 }
+
+    it "should be true if the product has the required fields" do
+      Buscalibre::Product.valid?(valid_product).should be true
+    end
+
+    it "should be false if the product has not the required fields" do
+      valid_product.delete :price
+      Buscalibre::Product.valid?(valid_product).should be false
+    end
   end
 end
